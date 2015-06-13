54961d0c5c6b44ab0b47a1930193153b3ee5fa7a
theodhori-dhiamanti <teo_dia@hotmail.com>
2015-06-13 11:30:15 -0400

    Working on UserAccount.Authenticate

---
+++ UserAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBusterPOS
+{
+    public class UserAccount : Person
+    {
+        #region "Private Data"
+        private string m_userAccountID;
+        private string m_Username;
+        private string m_Password;
+
+        #endregion
+        #region "Public Properties"
+
+        #endregion
+        public override void Print()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Authenticate(string u_user, string u_pass)
+        {
+           
+        }
+
+        private string credsToSha512(string Credentials)
+        {
+             System.Security.Cryptography.SHA512CryptoServiceProvider Encrypt = new System.Security.Cryptography.SHA512CryptoServiceProvider();
+            byte[] ByteString = System.Text.Encoding.ASCII.GetBytes(Credentials);
+            ByteString = Encrypt.ComputeHash(ByteString);
+            string Result = null;
+            foreach (byte bt in ByteString)
+            {
+                Result += bt.ToString("x2"); 
+            }
+            return Result.ToUpper();
+        
+        }
+        
+    }
+}
