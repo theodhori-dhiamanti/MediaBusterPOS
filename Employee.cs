649bd68b44978e62767af8e78c79c4e1707a63f7
theodhori-dhiamanti <teo_dia@hotmail.com>
2015-06-16 23:09:39 -0400

    Progress Employee

--- Employee.cs
+++ Employee.cs
@@ -13,6 +13,46 @@ namespace MediaBusterPOS
         private UserAccount m_objUserAccount;
 
         #endregion
+
+        #region "Public Properties"
+        public string JobTitle
+        {
+            get { return m_JobTitle; }
+            set { m_JobTitle = value; }
+        }
+
+        public UserAccount UserAccount
+        {
+            get { return m_objUserAccount; }
+            set { m_objUserAccount = value; }
+        }
+
+        public override DateTime BirthDate
+        {
+            get
+            {
+                return base.BirthDate;
+            }
+            set
+            {
+                if ((DateTime.Now.Year - base.BirthDate.Year) >= 16)
+                {
+                    base.BirthDate = value;
+                }
+                else
+                {
+                    throw new System.Exception("Employee must be 16 years or older");
+                }
+
+            }
+        }
+
+        #endregion
+        public event SecurityAlertEventHandler SecurityAlert;
+        public delegate void SecurityAlertEventHandler(string username, string password);
+
+
+
         public override bool Authenticate(string u_user, string u_pass)
         {
             throw new NotImplementedException();
