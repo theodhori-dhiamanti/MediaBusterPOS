d6ea473db8c4329efb9452e3c8e613be43d3b91f
theodhori-dhiamanti <teo_dia@hotmail.com>
2015-06-16 22:39:42 -0400

    EmployeeList
    Progress on employee

--- MediaBusterPOS/UserAccount.cs
+++ MediaBusterPOS/UserAccount.cs
@@ -15,8 +15,40 @@ namespace MediaBusterPOS
 
         #endregion
         #region "Public Properties"
+        public string UserAccountID
+        {
+            get { return m_userAccountID; }
+            set { m_userAccountID = value; }
+        }
+        public string Username
+        {
+            get { return m_Username; }
+            set { m_Username = value; }
+        }
+        public string Password
+        {
+            get { return m_Password; }
+            set { m_Password = value; }
+        }
+        #endregion
+        #region "Constructors"
+        public UserAccount()
+        {
+            this.m_userAccountID = "";
+            this.m_Username = "";
+            this.m_Password = "";
+        }
+        public UserAccount(string U_accountID, string U_Name, string U_pass )
+        {
+            this.UserAccountID = U_accountID;
+            this.Username = U_Name;
+            this.Password = U_pass;
+        }
 
         #endregion
+
+
+
         public override void Print()
         {
             throw new NotImplementedException();
@@ -41,5 +73,45 @@ namespace MediaBusterPOS
         
         }
         
+        public static UserAccount Create()
+        {
+            return DataPortal_Create();
+        }
+
+        public void Load(string Key)
+        {
+            DataPortal_Fetch(Key);
+        }
+        public void Save()
+        {
+
+        }
+
+        public void ImediateDelete(string Key)
+        {
+            DataPortal_Delete(Key);
+        }
+
+        protected static UserAccount DataPortal_Create()
+        {
+            return null;  //good for now, will update later
+        }
+        
+        protected void DataPortal_Fetch(string Key)
+        {
+
+        }
+
+        protected void DataPortal_Update()
+        {
+        }
+
+        protected void DataPortal_Insert()
+        {
+        }
+
+        protected void DataPortal_Delete(string Key)
+        {
+        }
     }
 }
